3308bcb01dc56f5b63c855e781cb68fa4af35654
Fu Diwei <fudiwei@sina.com>
2025-07-28 12:59:20 +0800

    dev

--- ui/src/utils/file.ts
+++ ui/src/utils/file.ts
@@ -1,4 +1,4 @@
-export function readFileContent(file: File): Promise<string> {
+export function readFileAsText(file: File): Promise<string> {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
 

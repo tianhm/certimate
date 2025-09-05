b7fbb21a96729f97c69d1fe3aaf36a7e93047201
Fu Diwei <fudiwei@sina.com>
2025-09-06 00:59:58 +0800

    feat: optimize deployment provider of huaweicloud obs

---
+++ internal/certdeploy/deployers/sp_huaweicloud_obs.go
@@ -0,0 +1,30 @@
+package deployers
+
+import (
+	"fmt"
+
+	"github.com/certimate-go/certimate/internal/domain"
+	"github.com/certimate-go/certimate/pkg/core"
+	huaweicloudobs "github.com/certimate-go/certimate/pkg/core/ssl-deployer/providers/huaweicloud-obs"
+	xmaps "github.com/certimate-go/certimate/pkg/utils/maps"
+)
+
+func init() {
+	if err := Registries.Register(domain.DeploymentProviderTypeHuaweiCloudOBS, func(options *ProviderFactoryOptions) (core.SSLDeployer, error) {
+		credentials := domain.AccessConfigForHuaweiCloud{}
+		if err := xmaps.Populate(options.ProviderAccessConfig, &credentials); err != nil {
+			return nil, fmt.Errorf("failed to populate provider access config: %w", err)
+		}
+
+		provider, err := huaweicloudobs.NewSSLDeployerProvider(&huaweicloudobs.SSLDeployerProviderConfig{
+			AccessKeyId:     credentials.AccessKeyId,
+			SecretAccessKey: credentials.SecretAccessKey,
+			Region:          xmaps.GetString(options.ProviderExtendedConfig, "region"),
+			Bucket:          xmaps.GetString(options.ProviderExtendedConfig, "bucket"),
+			Domain:          xmaps.GetString(options.ProviderExtendedConfig, "domain"),
+		})
+		return provider, err
+	}); err != nil {
+		panic(err)
+	}
+}

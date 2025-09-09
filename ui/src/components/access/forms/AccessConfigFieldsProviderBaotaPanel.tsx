586a11f91c8b5525a6aea1ebd9246bf05928f8d1
Fu Diwei <fudiwei@sina.com>
2025-09-09 09:39:09 +0800

    chore(ui): improve i18n

--- ui/src/components/access/forms/AccessConfigFieldsProviderBaotaPanel.tsx
+++ ui/src/components/access/forms/AccessConfigFieldsProviderBaotaPanel.tsx
@@ -40,12 +40,12 @@ const AccessConfigFormFieldsProviderBaotaPanel = () => {
       <Form.Item
         name={[parentNamePath, "allowInsecureConnections"]}
         initialValue={initialValues.allowInsecureConnections}
-        label={t("access.form.common_allow_insecure_conns.label")}
+        label={t("access.form.shared_allow_insecure_conns.label")}
         rules={[formRule]}
       >
         <Switch
-          checkedChildren={t("access.form.common_allow_insecure_conns.switch.on")}
-          unCheckedChildren={t("access.form.common_allow_insecure_conns.switch.off")}
+          checkedChildren={t("access.form.shared_allow_insecure_conns.switch.on")}
+          unCheckedChildren={t("access.form.shared_allow_insecure_conns.switch.off")}
         />
       </Form.Item>
     </>

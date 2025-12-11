4550058a909f33b5d348c2c6934621d830be2ff2
Fu Diwei <fudiwei@sina.com>
2025-12-11 17:58:33 +0800

    refactor: clean code

--- ui/src/components/workflow/designer/nodes/BizApplyNodeRegistry.tsx
+++ ui/src/components/workflow/designer/nodes/BizApplyNodeRegistry.tsx
@@ -31,7 +31,6 @@ export const BizApplyNodeRegistry: NodeRegistry = {
       ["config"]: ({ value }) => {
         const res = BizApplyNodeConfigForm.getSchema({}).safeParse(value);
         if (!res.success) {
-          console.log(res);
           return {
             message: res.error.message,
             level: FeedbackLevel.Error,

60fd563bf14c2d393111972c2e53ad2fded9061b
Fu Diwei <fudiwei@sina.com>
2026-01-06 21:49:07 +0800

    bump version to v0.4.13

--- ui/src/domain/app.ts
+++ ui/src/domain/app.ts
@@ -1,10 +1,11 @@
 ﻿import i18next from "i18next";
 
-// fallback policy: .env > git tag > "v0.0.0-dev"
-export const APP_VERSION: string = "v" + (__APP_VERSION__ || "0.0.0-dev").replace(/^v/, "");
+export const APP_VERSION = "v" + (__APP_VERSION__ || "0.0.0-dev").replace(/^v/, "");
 
 export const APP_REPO_URL = "https://github.com/certimate-go/certimate";
+
 export const APP_DOWNLOAD_URL = APP_REPO_URL + "/releases";
+
 export let APP_DOCUMENT_URL = "https://docs.certimate.me";
 
 i18next.on("languageChanged", (language) => {

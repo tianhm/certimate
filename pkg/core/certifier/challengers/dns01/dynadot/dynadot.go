51dfdf55bc4a5398c5b4b445e194df3f5cbea6bb
Fu Diwei <fudiwei@sina.com>
2026-05-27 23:47:14 +0800

    refactor(provider): use lego to implement dns-01 challenger of dynadot

--- pkg/core/certifier/challengers/dns01/dynadot/dynadot.go
+++ pkg/core/certifier/challengers/dns01/dynadot/dynadot.go
@@ -4,8 +4,9 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/go-acme/lego/v5/providers/dns/dynadot"
+
 	"github.com/certimate-go/certimate/pkg/core/certifier"
-	"github.com/certimate-go/certimate/pkg/core/certifier/challengers/dns01/dynadot/internal"
 )
 
 type ChallengerConfig struct {
@@ -20,7 +21,7 @@ func NewChallenger(config *ChallengerConfig) (certifier.ACMEChallenger, error) {
 		return nil, fmt.Errorf("the configuration of the acme challenge provider is nil")
 	}
 
-	providerConfig := internal.NewDefaultConfig()
+	providerConfig := dynadot.NewDefaultConfig()
 	providerConfig.APIKey = config.ApiKey
 	providerConfig.APISecret = config.ApiSecret
 	if config.DnsPropagationTimeout != 0 {
@@ -30,7 +31,7 @@ func NewChallenger(config *ChallengerConfig) (certifier.ACMEChallenger, error) {
 		providerConfig.TTL = config.DnsTTL
 	}
 
-	provider, err := internal.NewDNSProviderConfig(providerConfig)
+	provider, err := dynadot.NewDNSProviderConfig(providerConfig)
 	if err != nil {
 		return nil, err
 	}

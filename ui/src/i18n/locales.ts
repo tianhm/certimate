6d6b79f7e2dc8db6d95ae087ee71dc8fa3aa9d50
Fu Diwei <fudiwei@sina.com>
2026-06-19 11:28:26 +0800

    refactor: clean code

--- ui/src/i18n/locales.ts
+++ ui/src/i18n/locales.ts
@@ -1,6 +1,6 @@
 ﻿import { LANG_EN, LANG_ZH } from "./resources";
 
 export const localeNames = {
-  ZH: LANG_ZH,
   EN: LANG_EN,
+  ZH: LANG_ZH,
 };
--- ui/src/i18n/resources/index.ts
+++ ui/src/i18n/resources/index.ts
@@ -3,17 +3,17 @@ import { type Resource } from "i18next";
 import en from "./en";
 import zh from "./zh";
 
-export const LANG_ZH = "zh" as const;
 export const LANG_EN = "en" as const;
+export const LANG_ZH = "zh" as const;
 
 const resources: Resource = {
-  [LANG_ZH]: {
-    name: "简体中文",
-    translation: zh,
-  },
   [LANG_EN]: {
     name: "English",
     translation: en,
+  },
+  [LANG_ZH]: {
+    name: "简体中文",
+    translation: zh,
   },
 };
 
